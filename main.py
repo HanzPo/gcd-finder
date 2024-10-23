f0537c02e3ea3305d617799fd30707f49c6bd0b7
HanzPo <hanznathanpo@gmail.com>
2024-10-23 05:08:38 -0400

    Added Bezout's lemma thingy

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from math import floor
 
-print("Enter two integers to find the GCD of:")
+print("Enter two nonzero integers to find the GCD of:")
 
 a = int(input("a = "))
 b = int(input("b = "))
 
+if (b > a):
+  helper = a
+  a = b
+  b = helper
+
 def gcd(a, b):
+
+#  if (a == 0 and b == 0):
+#    return 0
+#  elif (a == 0):
+#    return abs(b)
+#  elif (b == 0):
+#    return abs(a)
+
   rows = []
   rows.append([1, 0, a, 0])
   rows.append([0, 1, b, 0])
@@ -19,7 +32,7 @@ def gcd(a, b):
     current_row.insert(0, rows[count - 2][2] % rows[count - 1][2])
     if (rows[count - 2][2] % rows[count - 1][2] == 0):
       running = False
-      return rows[count - 1][2]
+      return [rows[count - 1][0], rows[count - 1][1], rows[count - 1][2]]
     
     current_row.insert(0, rows[count - 2][1] - (rows[count - 1][1] * current_row[1]))
     current_row.insert(0, rows[count - 2][0] - (rows[count - 1][0] * current_row[2]))
@@ -27,7 +40,10 @@ def gcd(a, b):
     rows.append(current_row)
     count += 1
 
+  return []
 
-  return -1
+result = gcd(a, b)
 
-print(gcd(a, b))
+print(f"The GCD of {a} and {b} is {result[2]}")
+print("We can verify this using Bezout's Lemma")
+print(f"({result[0]}){a} + ({result[1]}){b} = {eval(f"({result[0]} * {a}) + ({result[1]} * {b})")}")
